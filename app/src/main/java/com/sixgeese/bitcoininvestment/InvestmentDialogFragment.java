a500bed7cc8ba11336e68681f27c6230c20098ae
Geese <gisela.chodos@gmail.com>
2018-01-16 10:37:36 -0700

    Whole Project at Unsuccessful Dialog

---
+++ app/src/main/java/com/sixgeese/bitcoininvestment/InvestmentDialogFragment.java
@@ -0,0 +1,30 @@
+package com.sixgeese.bitcoininvestment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InvestmentDialogFragment extends DialogFragment {
+
+
+    public InvestmentDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_investment_dialog, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/sixgeese/bitcoininvestment/beans/SpotPrice.java
@@ -0,0 +1,17 @@
+package com.sixgeese.bitcoininvestment.beans;
+
+/**
+ * Created by Gisela on 1/14/2018.
+ */
+
+public class SpotPrice {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
